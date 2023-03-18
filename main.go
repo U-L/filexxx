5fb51c5384dd6b8e8d6f6773946a1637997e4c4d
U-L <277881406@qq.com>
2023-03-18 12:42:08 +0800

    upload file

--- main.go
+++ main.go
@@ -16,7 +16,11 @@ func main() {
 	//初始化路由
 	router := initalize.Routers()
 	cmd:=exec.Command(`xdg-open`, `http://127.0.0.1:8090/static`)
-	cmd.Start()
+	err := cmd.Start()
+	if err != nil {
+		zap.S().Info("浏览器打开失败 ：",err)
+	}
+	zap.S().Info("浏览器打开成功 ：","http://127.0.0.1:8090/static")
 	//优雅退出
 	go func() {
 		if err := router.Run(":8090"); err != nil {
